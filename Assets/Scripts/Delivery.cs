b464f3575668a89d91fc0982d5412765d15a415b
AnuragNegii <negianurag99@gmail.com>
2022-11-26 11:35:33 +0530

    added GetComponent for spriteRendererColor

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     // private int noOfPackagesHeld;
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    [SerializeField] float delay = 0.5f;
     void OnCollisionEnter2D(Collision2D other) {
             Debug.Log("OOF its Gone wRonG");
     }
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Package"){
+        if(other.tag == "Package" && !hasPackage){
             // noOfPackagesHeld +=1;
             Debug.Log("Package picked");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject,delay);
+            
 
         }
         //&& noOfPackagesHeld > 0
         if(other.tag == "Customer" && hasPackage){
             Debug.Log("Package Delievered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
             // noOfPackagesHeld -=1;
         }
     }
